837dafadb142815e363e2ea4448bd624761017d1
Abhishek Kumar <abhishekk580@gmail.com>
2020-02-09 23:24:44 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ApiService } from './api.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,74 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crud';
+  movies = [
+    { title: "test" }
+  ];
+
+  selectedMovie;
+
+  constructor(private api: ApiService) {
+    this.getMovies();
+    this.selectedMovie = {
+      id: -1,
+      title: "",
+      desc: "",
+      year: 0
+    }
+  }
+
+  getMovies = () => {
+    this.api.getAllMovies().subscribe(
+      data => {
+        this.movies = data;
+      },
+      error => {
+        console.log(error)
+      }
+    );
+  }
+
+  movieClicked = (movie) => {
+    this.api.getOneMovies(movie.id).subscribe(
+      data => {
+        this.selectedMovie = data;
+      },
+      error => {
+        console.log(error)
+      }
+    );
+  }
+
+  updateMovie = () => {
+    this.api.updateMovie(this.selectedMovie).subscribe(
+      data => {
+        this.getMovies();
+      },
+      error => {
+        console.log(error)
+      }
+    );
+  }
+
+  createMovie = () => {
+    this.api.createMovie(this.selectedMovie).subscribe(
+      data => {
+        this.movies.push(data);
+      },
+      error => {
+        console.log(error)
+      }
+    );
+  }
+
+  deleteMovie = () => {
+    this.api.deleteMovie(this.selectedMovie.id).subscribe(
+      data => {
+        this.getMovies();
+      },
+      error => {
+        console.log(error)
+      }
+    );
+  }
 }
